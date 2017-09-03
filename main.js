0edd464d133a6c644c5f22bb0656b530a738c40d
gib <gibbowden@gmail.com>
2017-09-03 00:50:27 -0500

    added bootstrap

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+const blogContainer = document.getElementById("blog-container")
+const jumboContainerContent = document.getElementById("jumbo-container-content");
+const jumboDiv = jumboContainerContent.parentNode;
+
 var productsRequest = new XMLHttpRequest();
 	productsRequest.addEventListener("load", importBlog)
 	productsRequest.addEventListener("error", logFailedRequest)
@@ -13,20 +17,81 @@ function logFailedRequest(){
 	console.log("dis broke")
 }
 
+
 function blogBuilder(blogs) {
-	for (blog of blogs) {
-		var blogDomString =	    
-			`<div id="${blog.cardId}" class="blog-card">
+	let blogContent = "";
+	for (blog of blogs) {		
+		const blogDomString =	    
+			`<div id="${blog.cardId}" class="blog-card col-md-3">
 		      	<section class="blog-card-content">
 				    <div class="blog-title">
 			          	<h4>${blog.title}</h4>
 				        <h5>${blog.date}</h5>
 				    </div>
 				    <div class="blog-text">
-				        <p>${blog.entry}</p>
+				        <p class="entry">${blog.entry}</p>
 				    </div>
 				</section>
 			</div>`
-	document.getElementById("blog-container").innerHTML += blogDomString;
+		blogContent += blogDomString;
 	} 
+	blogContainer.innerHTML = blogContent;
 }
+
+blogContainer.addEventListener("click", (e) => {	
+	let cardContent;
+	if (e.target.parentNode.tagName === "BODY") {
+		return; 
+	}
+	else if (e.target.classList.contains("blog-card")) {
+		console.log(cardContent = e.target.children[0]);
+	}
+	else if (e.target.parentNode.classList.contains("blog-card")) {
+		console.log(cardContent = e.target);
+	}
+	else if (e.target.parentNode.parentNode.classList.contains("blog-card")) {
+		console.log(cardContent = e.target.parentNode)
+	}
+	else if (e.target.parentNode.parentNode.parentNode.classList.contains("blog-card")) {
+		console.log(cardContent = e.target.parentNode.parentNode);
+	}
+
+	jumboContainerContent.innerHTML = cardContent.innerHTML; //insert cardContent HTML into the jumbotron
+	jumboContainerContent.parentNode.classList.remove("hidden"); //remove the hidden class from the jumbotron 
+	jumboContainerContent.parentNode.scrollIntoView();
+})
+
+document.getElementById("close-jumbo-btn").addEventListener("click", () => {
+	jumboDiv.classList.add("hidden");
+	})
+
+//searchbox event listener for regular keypress
+document.getElementById("search-box").addEventListener("keypress", (e) => {
+	filterBlogsOnSearch(e.target.value + e.key);
+})
+
+//searchbox event listener to account for backspace
+document.getElementById("search-box").addEventListener("keydown", (e) => {
+	console.log(e)
+	if (e.key === "Backspace") {
+		filterBlogsOnSearch(e.target.value.slice(0, -1))
+	}
+})
+
+document.getElementById("search-box").addEventListener("click", () => {
+	jumboDiv.classList.add("hidden");
+})
+
+
+const filterBlogsOnSearch = (str) => {
+	let filteredBlogs = blogs.filter((blog) => {
+		return (blog.entry.indexOf(str) > -1); 
+	})
+	blogBuilder(filteredBlogs);
+}
+
+
+
+
+
+
